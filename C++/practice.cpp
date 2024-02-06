2ed04e43e44e20733f1327336f8b185198623255
nollae <dudghk0924@naver.com>
2024-02-06 23:55:22 +0900

    C++에 대하여

---
+++ C++/practice.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> v;
+vector<vector<int>> v2(10, vector<int>(10,0));
+vector<int> v3[10];
+
+int main(){
+    
+    for(int i = 0; i < 10; i++){
+        vector<int> temp;
+        v.push_back(temp);
+    }
+
+    return 0;
+
+}
