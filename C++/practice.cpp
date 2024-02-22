9178923db9394dadd7cbe9e9b0c698a04c119e62
nollae <dudghk0924@naver.com>
2024-02-22 22:10:53 +0900

    02. dfs

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,28 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int n = 3; 
-int a[n][n], visited[n][n];
+
+int n, m, ret;
+int a[104][104];
+bool visited[104][104];
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
 
-void go(int y, int x){
+void dfs(int y, int x){
     visited[y][x] = 1;
     cout << y << " : " << x << "\n";
     for(int i = 0; i < 4; i++){
-        int ny = y + dy[i]; 
+        int ny = y + dy[i];
         int nx = x + dx[i];
-        if(ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
-        if(a[ny][nx] == 0) continue;
-        if(visited[ny][nx]) continue;
-        go(ny, nx);
+        if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+        if(a[ny][nx] && !visited[ny][nx]) {
+            dfs(ny, nx);
+        }
     }
-    return; 
 }
-int main(){  
+
+int main(){
+    cin >> n >> m;
+
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             cin >> a[i][j];
         }
     }
-    go(0, 0); 
-} 
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(a[i][j] && !visited[i][j]){
+                ret++;
+                dfs(i, j);
+            }
+        }
+    }
+
+    cout << ret;
+}
