77927fe0b9c91c077aee61c9ab72dc1d9cd0e371
nollae <dudghk0924@naver.com>
2024-02-24 07:05:47 +0900

    02. bfs

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,41 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, ret;
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
-int a[104][104], visited[104][104];
+vector<int> a[100];
+int visited[100];
+int nodeList[] = {10, 12, 14, 16, 18, 20, 22, 24};
 
-void dfs(int y, int x){
-    visited[y][x] = 1;
-    cout << y << " : " << x << "\n";
-    for(int i = 0; i < 4; i++){
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-        if(visited[ny][nx]) continue;
-        if(!a[ny][nx]) continue;
-        dfs(ny, nx);
+void bfs(int here){
+    queue<int> q;
+    visited[here] = 1;
+    q.push(here);
+
+    while(q.size()){
+        int here = q.front();
+        q.pop();
+        for(int there : a[here]){
+            if(visited[there]) continue;
+            visited[there] = visited[here] + 1;
+            q.push(there);
+        }
     }
+    
 }
 
 int main(){
-    cin >> n >> m;
+    a[10].push_back(12);
+    a[10].push_back(14);
+    a[10].push_back(16);
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            cin >> a[i][j];
-        }
+    a[12].push_back(18);
+    a[12].push_back(20);
+
+    a[20].push_back(22);
+    a[20].push_back(24);
+
+    bfs(10);
+
+    for(int i : nodeList){
+        cout << i << " : " << visited[i] << "\n";
     }
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(a[i][j] && visited[i][j] == 0){
-                ret++;
-                dfs(i, j);
-            } 
-        }
-    }
+    cout << "최단 거리 : " << visited[24] - 1;
 
-    cout << ret;
 }
