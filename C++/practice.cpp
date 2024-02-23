d5d5e797b157965bfdcee9f4a764a4ecf4e13e27
nollae <dudghk0924@naver.com>
2024-02-24 00:28:40 +0900

    02. 트리순회

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,44 +1,60 @@
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std; 
+vector<int> adj[1004]; 
+int visited[1004];
 
-int n, m, sy, sx, ty, tx;
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
-int a[104][104], visited[104][104];
+void postOrder(int here){  
+    if(visited[here] == 0){
+        if(adj[here].size() == 1) postOrder(adj[here][0]);
+        if(adj[here].size() == 2){
+            postOrder(adj[here][0]);
+            postOrder(adj[here][1]);
+        }
+        visited[here] = 1;
+        cout << here << " ";
+    }
+} 
 
-void dfs(int y, int x){
-    queue<pair<int, int>> q;
-    visited[y][x] = 1;
-    q.push({y, x});
-
-    while(q.size()){
-        tie(y, x) = q.front(); 
-        q.pop();
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-            if(!a[ny][nx] || visited[ny][nx]) continue;
-            visited[ny][nx] = visited[y][x] + 1;
-            q.push({ny, nx});
+void preOrder(int here){ 
+    if(visited[here] == 0){
+        visited[here] = 1;
+        cout << here << " ";
+        if(adj[here].size() == 1) preOrder(adj[here][0]);
+        if(adj[here].size() == 2){
+            preOrder(adj[here][0]);
+            preOrder(adj[here][1]);
         }
     }
-}
+}  
 
+void inOrder(int here){    
+    if(visited[here] == 0){
+        if(adj[here].size() == 1){
+            inOrder(adj[here][0]);
+            visited[here] = 1;
+            cout << here << " ";
+        }else if(adj[here].size() == 2){
+            inOrder(adj[here][0]);
+            visited[here] = 1;
+            cout << here << " ";
+            inOrder(adj[here][1]);
+        }else{
+            visited[here] = 1;
+            cout << here << " ";
+        }
+    }
+} 
 int main(){
-
-    cin >> n >> m;
-    cin >> sy >> sx;
-    cin >> ty >> tx;
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            cin >> a[i][j];
-        }
-    }
-
-    dfs(sy, sx);
-
-    cout << visited[ty][tx];
-
-}
+	adj[1].push_back(2);
+	adj[1].push_back(3);
+	adj[2].push_back(4);
+	adj[2].push_back(5); 
+	int root = 1;
+    cout << "\n 트리순회 : postOrder \n";
+    postOrder(root); memset(visited, 0, sizeof(visited));
+    cout << "\n 트리순회 : preOrder \n"; 
+    preOrder(root); memset(visited, 0, sizeof(visited)); 
+    cout << "\n 트리순회 : inOrder \n"; 
+    inOrder(root); 
+    return 0;
+} 
