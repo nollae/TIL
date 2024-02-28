97924ed02c55292fc9c8b7c5eeb6a1f3e7753847
nollae <dudghk0924@naver.com>
2024-02-28 09:42:06 +0900

    03. 완탐과 원복

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,33 +1,44 @@
 #include <bits/stdc++.h>
-using namespace std;
+using namespace  std;
 
-int n, temp, ret;
-vector<int> v;
-const int mod = 11;
+const int n = 3;
+int a[3][3] = {
+{10, 20, 21}, 
+{70, 90, 12},
+{80, 110, 120} 
+};
+int visited[3][3];
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1};
+vector<int> v; 
 
-void go(int idx, int sum){
-    if(ret == 10) return;
-    if(idx == n){
-        ret = max(ret, sum % mod);
+void print(){
+    for(int i : v) cout << i << " ";
+	cout << '\n';
+}
+
+void go(int y, int x){
+    if(y == n - 1 && x == n - 1){
+        print();
         return;
     }
-
-    go(idx + 1, sum + v[idx]);
-    go(idx + 1, sum);
+    for(int i = 0; i < 4; i++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(ny < 0 || nx < 0 || ny >= 3 || ny >= 3) continue;
+        if(visited[ny][nx]) continue;
+        visited[ny][nx] = 1;
+        v.push_back(a[ny][nx]);
+        go(ny, nx);
+        visited[ny][nx] = 0;
+        v.pop_back();
+    }
 }
 
 int main(){
-    cin >> n;
 
-    for(int i = 0; i < n; i++){
-        cin >> temp;
-        v.push_back(temp);
-    }
-
+    visited[0][0] = 1;
+    v.push_back(a[0][0]);
     go(0, 0);
-
-    cout << ret << "\n";
-    cout << ret << "\n";
-
     return 0;
 }
