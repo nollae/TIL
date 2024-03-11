8444e87a5ce91a452c5c81363b22682961ae5030
nollae <dudghk0924@naver.com>
2024-03-11 13:27:20 +0900

    12851 문제풀이

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,44 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, ret = -987654321;
-string s;
-vector<int> num;
-vector<char> oper;
-
-int oper_(char a, int b, int c){
-    if(a == '+') return b + c;
-    if(a == '-') return b - c;
-    if(a == '*') return b * c;
-}
-
-void go(int here, int _num){
-    if(here == num.size() - 1){
-        ret = max(ret, _num);
-        return;
-    }
-
-    go(here+1, oper_(oper[here], _num, num[here + 1]));
-
-    if(here + 2 <= num.size() - 1){
-        int temp = oper_(oper[here + 1], num[here + 1], num[here + 2]);
-        go(here + 2, oper_(oper[here], _num, temp));
-    }
-
-    return;
-}
+int n, k;
+int visited[100004];
+long long cnt[100004];
 
 int main(){
-    cin >> n;
-    cin >> s;
+    cin >> n >> k;
+    if(n == k){
+        puts("0"); puts("1");
+        return 0;
+    }   
 
-    for(int i = 0; i < n; i++){
-        if(i % 2 == 0) num.push_back(s[i] - '0');
-        else oper.push_back(s[i]);
+    visited[n] = 1;
+    cnt[n] = 1;
+    queue<int> q;
+    q.push(n);
+
+    while(q.size()){
+        int now = q.front();
+        q.pop();
+        for(int next : {now-1, now+1, now*2}){
+            if (0 <= next && next <= 100000) { 
+                if(!visited[next]){
+                    q.push(next);
+                    visited[next] = visited[now] + 1;
+                    cnt[next] += cnt[now];
+                }else if(visited[next] == visited[now] + 1){
+                    cnt[next] += cnt[now];
+                }
+            }
+        }
     }
 
-    go(0, num[0]);
+    cout << visited[k] - 1 << '\n';
+    cout << cnt[k] << '\n';
 
-    cout << ret;
-
+    return 0;
 }
