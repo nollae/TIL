9ea8f964650ef6250b64c42baeecad7650c6d7a2
nollae <dudghk0924@naver.com>
2024-03-28 13:33:51 +0900

    ~ 문제풀이

--- C++/02_알고리즘개념및문제/04_비트마스킹/04_02_J_14391_0.cpp
+++ C++/02_알고리즘개념및문제/04_비트마스킹/04_02_J_14391_0.cpp
@@ -1,25 +1,23 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
 using namespace std;
-
-int a[4][4], n, m, ret;
-
-int main(){
-    cin >> n >> m;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            cin >> a[i][j];
+int a[4][4], n, m, ret; 
+int main() { 
+    scanf("%d %d",&n,&m);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%1d",&a[i][j]);
         }
-    }
-
-    for(int s = 0; s < (1 << (n*m)); s++){
+    } 
+    for (int s = 0; s < (1 << (n * m)); s++) {
         int sum = 0;
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             int cur = 0;
-            for(int j = 0; j < m; j++){
+            for (int j = 0; j < m; j++) {
                 int k = i * m + j;
-                if((s & (1 << k)) == 0){
+                if ((s & (1<<k)) == 0) {
                     cur = cur * 10 + a[i][j];
-                }else{
+                } else {
                     sum += cur;
                     cur = 0;
                 }
@@ -39,8 +37,8 @@ int main(){
             }
             sum += cur;
         }
-        ret = max(ret, sum);
+        ret = max(ret,sum);
     }
-
-     cout << ret;
+    cout << ret << '\n';
+    return 0;
 }
